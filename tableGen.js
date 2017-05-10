a92b577c3ff439b59d5e5f42ce9a1d9215e6b5c8
Sebastien <seb.mandaba@gmail.com>
2017-05-10 13:37:44 +0200

    TD created

--- tableGen.js
+++ tableGen.js
@@ -3,9 +3,15 @@ function trGen(text) {
     let tr = document.createElement("tr");
     tr.id = "tr" + a;
     tr.innerHTML = text;
-    select = document.body.querySelector("#table");
+    select = document.body.querySelector("#tdValeur");
     select.appendChild(tr);
     console.log("Tr " + a + " added")
+
+    /*let trVal = document.createElement("tr");
+    trVal.id = "trVal" + a;
+    trVal.innerHTML = a;
+    select = document.body.querySelector("#tdIndex");
+    select.appendChild(trVal);*/
 }
 
 let arr = ["AAA", "BBB", "CCC", "DDD", "EEE", "FFF"];
@@ -15,7 +21,13 @@ let table = document.createElement("table");
 table.id = "table";
 before = document.body;
 before.appendChild(table);
+let tdIndex = document.createElement("td");
+tdIndex.id = "tdIndex";
+document.body.querySelector("#table").appendChild(tdIndex);
 
+let tdValeur = document.createElement("td");
+tdValeur.id = "tdValeur";
+document.body.querySelector("#table").appendChild(tdValeur);
 for (i = 0; i != arr.length; i = i + 1) {
     trGen(arr[i]);
 }
